c910776e588a54c46b94098cb04fe6369530d0b2
Lennart Kloock <lennart.kloock@protonmail.com>
2026-08-06 10:49:04 +0200

    feat: add subcommand to work

--- fish/functions/work.fish
+++ fish/functions/work.fish
@@ -1,18 +1,31 @@
-function work -a week_diff --description 'show work in week starting on thursday'
-    if test -z $week_diff
-        set week_diff 0
+function work -a action week_diff --description 'work util command related to time tracking'
+    if test -z $action
+        set action work
     end
 
-    if test (date +%u) = 4
-        set start (date --date="thursday" +%s)
+    if test "$action" = work
+        timew start
+        ssh kokuna
+        timew stop
+        timewsync
+    else if test "$action" = show
+        if test -z $week_diff
+            set week_diff 0
+        end
+
+        if test (date +%u) = 4
+            set start (date --date="thursday" +%s)
+        else
+            set start (date --date="last thursday" +%s)
+        end
+        set end (date --date="next thursday" +%s)
+
+        set diff (math "$week_diff * 7 * 24 * 60 * 60")
+        set start (math $start - $diff)
+        set end (math $end - $diff)
+
+        timew week (date --iso-8601=seconds --date="@$start") - (date --iso-8601=seconds --date="@$end")
     else
-        set start (date --date="last thursday" +%s)
+        echo "Unknown action: $action"
     end
-    set end (date --date="next thursday" +%s)
-
-    set diff (math "$week_diff * 7 * 24 * 60 * 60")
-    set start (math $start - $diff)
-    set end (math $end - $diff)
-
-    timew week (date --iso-8601=seconds --date="@$start") - (date --iso-8601=seconds --date="@$end")
 end
